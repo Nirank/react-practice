d0ffcd6afa8338fee5d0aedcafc10bce4ba27a64
Nirank <jawalenirank98@gmail.com>
2024-01-17 10:41:51 +0530

    currency converter

---
+++ currencyconverter/src/components/Inputbox.js
@@ -0,0 +1,95 @@
+import React, {useId} from 'react'
+
+function Inputbox({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectedCurrency = "usd",
+    amountDisabled = false,
+    currrencyDisabled = false,
+    className = "",
+}) {
+
+    const id = useId()
+  return (
+    <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+        <div className='w-1-2'>
+            <label htmlFor={id}  className='text-black/40 mb-2 inline-block'>{label}</label>
+            <input 
+            id={id}
+            type="number"
+            className='outline-none w-full bg-transparent py-1.5'
+            placeholder='Amount'
+            disabled={amountDisabled}
+            value={amount}
+            onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+             />
+        </div>
+        <div className='w-1/2 flex flex-wrap justify-end text-right'>
+        <p className="text-black/40 mb-2 w-full">Currency Type</p>
+        <select 
+        className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+        value={selectedCurrency}
+        onChange={(e) => { onCurrencyChange && onCurrencyChange(e.target.value)}}
+        disabled={currrencyDisabled}
+        >
+            {currencyOptions.map((currency) => (
+                <option key={currency} value={currency}>{currency}</option>
+            ))}
+        </select>
+        </div>
+    </div>
+  )
+}
+
+export default Inputbox
+
+
+
+// import React from 'react'
+
+// function Inputbox([
+//     lable,
+//     amount ,
+//     onAmountChange,
+//     onCurrencyChange,
+//     currencyoptions = [],
+//     selectedCurrency = 'usd',
+//     amountDisable = false,
+//     currencyDisable = false,
+//     className = "",
+// ]) {
+//   return (
+//     <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
+//         <div className="w-1-2">
+//             <label htmlFor='currency' className='text-black/40 mb-2 inline-block'></label>
+//             debugger
+//             <input type="number"
+//                  className='outline-none w-full bg-transparent py-1.5' 
+//                  placeholder="Amount"
+//                  disabled={amountDisable}
+//                  value={amount}
+//                 //  onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+//                  name="" id="currency" />
+//         </div>
+//         <div className='w-1/2 flex flex-wrap justify-end text-right'>
+//             <p className='text-black/40 mb-2 w-full'>Currency Type</p>
+//             <select 
+//             className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+//             value={selectedCurrency} 
+//             onChange={ (e) => { onCurrencyChange && onCurrencyChange(e.target.value)}}
+//             disabled = {currencyDisable}
+//             id="">
+
+//                 {currencyoptions.map((currency) =>(
+//                     <option value={currency} key={currency}>{currency}</option>
+//                 ))}
+//             </select>
+//         </div>
+//     </div>
+//   )
+// }
+
+// export default Inputbox
--- passwordgenerator/src/App.js
+++ passwordgenerator/src/App.js
@@ -1,9 +1,83 @@
 import './App.css';
+import React, { useState , useCallback, useEffect, useRef } from'react';
 
 function App() {
+  const [length , setLength] = useState(8)
+  const [numberAllowed , setNumberAllowed] = useState(false)
+  const [charAllowed, setCharAllowed ] = useState(false)
+  const [password , setPassword ] = useState('')
+
+
+const passRef = useRef(null)
+
+
+  const generatePassword = useCallback( () => {
+    let newPassword = ''
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_+-=[]{}|;:,./<>?"
+    for(let i = 0; i < length; i++) {
+      newPassword += str.charAt(Math.floor(Math.random() * str.length))
+    }
+    setPassword(newPassword)
+  }, [length,  numberAllowed, charAllowed])
+
+
+useEffect (() => {
+  generatePassword()
+}, [length, numberAllowed, charAllowed])
+
+
+const copyPasswordToClipboard = () => {
+  window.navigator.clipboard.writeText(password)
+  passRef.current?.select()
+}
+
   return (
-    <div >
-      <h1>Hello</h1>
+    <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500 ' >
+      <h1 className='text-white text-center my-3'>Password Generator</h1>
+      <div className='flex shadow rounded-lg overflow-hidden mb-4'>
+        <input type='text' 
+        className='outline-none w-full py-1 px-3' 
+        value={password} 
+        placeholder='Password'
+         readOnly
+         ref={passRef}         
+         />
+        <button onClick={ copyPasswordToClipboard}
+        className='outline-none bg-blue-700 text-white px-3 py-1 shrink-0'>copy</button>
+      </div>
+
+      <div className='flex text-sm gap-x-2'>
+        <div className = 'flex items-center gap-x-1'>
+          <input type='range' 
+                 min={6}
+                 max={100}
+                 value={length}
+                 onChange={(e) => setLength(e.target.value)}
+                 name = '' id=''  className='outline-none' />
+
+                  <label htmlFor='length' className='text-white'>Length : {length}</label>
+        </div>
+{/* NUmber CheckBox */}
+        <div className = 'flex items-center gap-x-1'>
+          <input type='checkbox' 
+                 defaultChecked={numberAllowed}
+                 onChange={() => setNumberAllowed( (prev) => !prev)}
+                 name = '' id=''  className='outline-none' />
+
+                  <label htmlFor='number' className='text-white'>Number</label>
+        </div>
+{/* Char checkboxes */}
+        <div className = 'flex items-center gap-x-1'>
+          <input type='checkbox' 
+                 defaultChecked={charAllowed}
+                 onChange={() => setCharAllowed( (prev) => !prev)}
+                 name = '' id=''  className='outline-none' />
+
+                  <label htmlFor='chars' className='text-white'>Characters</label>
+        </div>
+      </div>
     </div>
   );
 }
