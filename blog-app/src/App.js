80f029908574573be7023841676b9e6954430585
Nirank <jawalenirank98@gmail.com>
2024-01-19 17:10:05 +0530

    Add Services for database and storage

---
+++ blog-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-yellow-300">
+     <h1 className ='text-red-500 '>Nirank</h1>
+    </div>
+  );
+}
+
+export default App;
